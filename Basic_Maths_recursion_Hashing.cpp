5d482afa471407c97235bb9985354eb0948d2c95
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2025-07-28 23:49:52 +0530

    Update Basic_Maths_recursion_Hashing.cpp
    day 2

--- Basic_Maths_recursion_Hashing.cpp
+++ Basic_Maths_recursion_Hashing.cpp
@@ -1,1 +1,6 @@
-
+//Extracting a digit 
+// The modulus operator % computes the remainder when a number is divided by another number. When we perform N % 10 we get the last digit of the number N.
+// Eg. N = 7789, then N%10 results in 9 which is the last digit.
+// After extracting the last digit we need to remove the last digit as well which can be done via division operator.
+// Eg. N = 7789 then N/10 results in 778, removing the last digit.
+// 
