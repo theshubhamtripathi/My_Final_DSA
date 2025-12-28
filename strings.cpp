b5c58de239a57fcb06cb0ec51878373f7d38f93e
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2025-12-29 02:53:49 +0530

    Update strings.cpp
    array rotation

--- strings.cpp
+++ strings.cpp
@@ -39,3 +39,23 @@ public:
 
     }
 };
+
+
+//left roatate array
+reverse(nums.begin(), nums.begin() + k);
+reverse(nums.begin() + k, nums.end());
+reverse(nums.begin(), nums.end());
+
+
+//right rotate array
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n;   // handle k > n
+
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+
+//
