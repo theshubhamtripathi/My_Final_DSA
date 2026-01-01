35240931722a39ce6341ebe32b8efd749eb02988
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-01-01 15:05:06 +0530

    Update strings.cpp
    spiral done

--- strings.cpp
+++ strings.cpp
@@ -321,4 +321,50 @@ public:
 
 
 //rotate matrix in spriL ORDER 
+//The 2 if conditions prevent duplicate traversal when the matrix reduces to a single row or column during spiral traversal.
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while(top <= bottom && left <= right){
+
+            // top row
+            for(int i = left; i <= right; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            // right column
+            for(int i = top; i <= bottom; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            // bottom row
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // left column
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+
+
 
