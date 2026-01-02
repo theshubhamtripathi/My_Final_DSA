11de2c9f0cb216b39e310b4253c2b04f0e1fce09
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-01-02 16:39:36 +0530

    Update mainstring.cpp
    remove outer paren done

--- mainstring.cpp
+++ mainstring.cpp
@@ -46,3 +46,26 @@ public:
         return nums.back();
     }
 };
+
+//remove outermost parentheses
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int count = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == ')'){
+                count--;
+            }
+            if(count != 0){
+                ans.push_back(s[i]);
+            }
+            if(s[i] == '('){
+                count++;
+            }
+        }
+        return ans;
+    }
+};
+
+//
