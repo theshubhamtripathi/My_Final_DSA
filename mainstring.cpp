79118eaabe1eaca8b604d594ad65c5e0600fe400
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-01-29 00:21:14 +0530

    Update mainstring.cpp
    leetcode 12 done

--- mainstring.cpp
+++ mainstring.cpp
@@ -370,3 +370,23 @@ public:
     }
 };
 
+//leetcode 12 
+class Solution {
+public:
+    vector<int> val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    vector<string> sym = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    string intToRoman(int num) {
+        string r = "";
+        for(int i=0;i<13;i++){
+            if(num == 0) break;
+            int times = num/val[i];
+            while(times--){
+                r += sym[i];
+            }
+            num %= val[i];
+        }
+        return r;
+    }
+};
+
+//leetcode 13
