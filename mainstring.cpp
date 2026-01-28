e8b93ac4fb4df50d0b6633d8fc9c23abd7d58be9
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-01-28 17:33:51 +0530

    Update mainstring.cpp
    leetcode 1614 done

--- mainstring.cpp
+++ mainstring.cpp
@@ -349,3 +349,24 @@ public:
 };
 
 //same question by using heaps 
+
+//leetcode 1614
+class Solution {
+public:
+    int maxDepth(string s) {
+        int maxnested = 0;
+        int count = 0;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            if(s[i] == '('){
+                count++;
+            }
+            else if(s[i] == ')'){
+                count--;
+            }
+            maxnested = max(count,maxnested);  //compare the number of count and maxnested and return what is max also as above it increases adter every opening bracket and decrrwasing after evevry closing bracket
+        }
+        return maxnested;
+    }
+};
+
