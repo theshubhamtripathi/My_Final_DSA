b1767965c4328d5f3ace294b81e66e91c76e1ba5
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-02-02 14:34:42 +0530

    Update mainstring.cpp
    map done

--- mainstring.cpp
+++ mainstring.cpp
@@ -707,3 +707,21 @@ public:
 // Useful when characters are not limited to only 26 letters
 // Complexity:
 // O(n² × unique_chars) → Slight slower than array
+        int n = s.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            unordered_map<char,int> mp;
+            for(int j=i;j<n;j++){
+                mp[s[j]]++;
+
+                int minf = INT_MAX;
+                int maxf = 0;
+
+                for(auto it:mp){
+                    minf = min(minf,it.second);
+                    maxf = max(maxf,it.second);
+                }
+                sum+=(maxf-minf);
+            }
+        }
+        return sum;
