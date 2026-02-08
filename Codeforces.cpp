8516fe0791faa40956d256b47a690e5d2f782a76
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-02-08 21:53:58 +0530

    Update Codeforces.cpp

--- Codeforces.cpp
+++ Codeforces.cpp
@@ -1,1 +1,25 @@
+//cp31 900 problem 3
+//This solution wants answer in logn or 1 not even n so from this we can get the idea we have to use some mathmatical formula 
+//just we have to implement one maths formula that is sum till n numbers that is n*(n-1)/2
+//<bits/stdc++.h> this is used to import all libraraies at once 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int q;
+    cin>>q;
+    while(q--){
+        long long n,k,x;
+        cin>>n>>k>>x;
+        long long minsum = k*(k+1)/2;
+        long long maxsum = n*(n+1)/2 - (n-k)*(n-k+1)/2;
+        if(x>=minsum && x<=maxsum){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
+
 
