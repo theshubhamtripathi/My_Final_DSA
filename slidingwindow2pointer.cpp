d2c11ca35f5150c8ecf84983a9a935dd6d0d01fe
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-02-15 10:14:26 +0530

    Update slidingwindow2pointer.cpp
    leetcode ptod

--- slidingwindow2pointer.cpp
+++ slidingwindow2pointer.cpp
@@ -1,3 +1,35 @@
+//leetcode 67 thoery neeche hai 
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int n = a.size()-1;
+        int m = b.size()-1;
+        string ans = "";
+        int sum = 0;
+        int carry = 0;
+
+        while(m>=0 || n>=0){
+            sum = carry;
+            if(n>=0){
+                sum += a[n] - '0';   //this will convert into integer value 
+                n--;
+            }
+            if(m>=0){
+                sum += b[m] - '0';
+                m--;
+            }
+
+            ans.push_back((sum%2==0) ? '0' : '1');
+            carry = (sum>1) ? 1 : 0; //it means if sum is greter than 1 then 1 else 0
+        }
+    if(carry > 0){
+            ans.push_back('1');
+        }
+    reverse(ans.begin(),ans.end());    
+    return ans;
+    }
+};
+
 1.) Constant Window 
 
 
@@ -101,8 +133,9 @@ int main(){
     return 0;
 }
 
+tc = O(n+n)     
 //this only works when All numbers are positive
 
 
-Optimal : 
+Optimal : tc = O(n)
     
