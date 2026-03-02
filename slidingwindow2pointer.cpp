90b7910d2965a51de7f9cdc739f58687ce5e93e9
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-03-02 15:35:45 +0530

    Update slidingwindow2pointer.cpp
    leetcode 930

--- slidingwindow2pointer.cpp
+++ slidingwindow2pointer.cpp
@@ -608,5 +608,32 @@ public:
         return r;
     }
 };
+
+//leetcode 930
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        //brute
+        int n = nums.size();
+        int count = 0;
+        //generate all subraays
+        // for(int i=0;i<n;i++){
+        //     int sum = 0;
+        //     for(int j=i;j<n;j++){
+        //         sum += nums[j];
+        //         if(sum == goal){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //better
+        
+
+    }
+};
+
 //leetcode 1248
+
 
