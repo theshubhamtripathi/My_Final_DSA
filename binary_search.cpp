5cce15d8b1387401b07936915e15e201bd1bafaf
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-05-05 00:01:46 +0530

    Update binary_search.cpp
    leetcode 35 done

--- binary_search.cpp
+++ binary_search.cpp
@@ -27,3 +27,24 @@ public:
 // Using this formula prevents Integer Overflow
 int mid = low + (high - low) / 2;
 
+
+//leetcode 35
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int high = n-1;
+        int low = 0;
+        while(low<=high){
+            int mid = (high+low)/2;
+            if(target == nums[mid]) return mid;
+            else if(target<nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
