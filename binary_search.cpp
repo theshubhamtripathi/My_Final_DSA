dd3b5cc3678cb95474da17136526b3070b896aa7
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-05-04 17:29:00 +0530

    Update binary_search.cpp
    leetcode 704 done

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,1 +1,29 @@
+//linear search in this we traverse from first to last element step by step 
+//Binary search is only applicable when the array is sorted 
+//tc : O(log(n)
+
+//leetcode 704
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int high = n-1;
+        int low = 0;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(target == nums[mid]) return mid;
+            else if(target<nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return -1;//if not exist
+    }
+};
+
+// Using this formula prevents Integer Overflow
+int mid = low + (high - low) / 2;
 
