a626ed34cdf2a7351e7f957d61863525744c556a
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-06-21 19:50:06 +0530

    Update Stacks and queue.cpp

--- Stacks and queue.cpp
+++ Stacks and queue.cpp
@@ -461,3 +461,58 @@ public:
     }
 };
 
+leetcode 155 min stack 
+class MinStack {
+private:
+    stack<long long> st;
+    long long mini;
+
+public:
+    MinStack() {
+        mini = INT_MAX;
+    }
+    
+    void push(int value) {
+        long long val = value;
+
+        if (st.empty()) {
+            st.push(val);
+            mini = val;
+        }
+        else {
+            if (val >= mini) {
+                st.push(val);
+            }
+            else {
+                st.push(2 * val - mini);
+                mini = val;
+            }
+        }
+    }
+    
+    void pop() {
+        if (st.empty()) {
+            return;
+        }
+
+        long long x = st.top();
+        st.pop();
+
+        if (x < mini) {
+            mini = 2 * mini - x;
+        }
+    }
+    
+    int top() {
+        long long x = st.top();
+
+        if (x < mini) {
+            return mini;
+        }
+        return x;
+    }
+    
+    int getMin() {
+        return mini;
+    }
+};
