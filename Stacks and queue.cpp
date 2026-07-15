d071489f2671bbc492f3ec2a60d4bd98a84fc44a
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-07-15 22:36:03 +0530

    Update Stacks and queue.cpp
    Leetcode 225 done

--- Stacks and queue.cpp
+++ Stacks and queue.cpp
@@ -703,6 +703,69 @@ int main() {
 }
 
 
+//Leetcode 225
+//There are 2 methods to solve this question first by 2 queues and 2nd by using only one queue.
+
+//We can also solve this question one queue also this is the ioslo up question for this concept 
+class MyStack {
+public:
+
+    //queue<int> q1;
+    //queue<int> q2;
+
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // swap(q1,q2);
+
+        q.push(x);
+        int n = q.size();
+        for(int i=0;i<n-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+        
+    }
+    
+    int pop() {
+        // int r = q1.front();
+        // q1.pop();
+        // return r;
+
+        int r = q.front();
+        q.pop();
+        return r;
+    }
+    
+    int top() {
+        // return q1.front();
+
+        return q.front();
+    }
+    
+    bool empty() {
+        // return q1.empty();
+
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
 
 
 
