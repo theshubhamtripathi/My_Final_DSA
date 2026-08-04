825b87d26883bb863a535e0f8bd1c0812792b8bd
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-08-04 22:15:00 +0530

    Update Linked_list.cpp

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -873,7 +873,38 @@ public:
 };
 
 Leetcode 234
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+//Just push karte jaao stack me and and phir compare karte karte nikalo
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+       stack<int> st;
+       ListNode* temp = head;
+       while(temp != nullptr){
+        st.push(temp->val);
+        temp = temp->next;
+       } 
 
+       temp = head;
+       while(temp != nullptr){
+        if(st.top() != temp->val){
+            return false;
+        }
+        st.pop();
+        temp = temp->next;
+       }
+       return true;
+    }
+};
 
 
 
