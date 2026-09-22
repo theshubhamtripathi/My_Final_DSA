fbd695417be5cb9d109b170430368c1f769cc383
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-09-22 16:24:55 +0530

    Update Dynamic Programming.cpp
    2d dp done

--- Dynamic Programming.cpp
+++ Dynamic Programming.cpp
@@ -312,7 +312,47 @@ public:
     }
 };
 
+// 2-d dp unique paths 
+Whenever someone says all possible ways use recursion 
+Steps to write the recurrence relation for a 2d dp:
+1.) Try to write everything in the form of indeexes in 2-d dp in the form of i and j
+2.) do all stuffs on those index (using by creating a small tree like structure chekcing the aloowed values and all in the qquestion)
+3.) return the ans as asked 
 
+to count the ways we have to write a base case we have return 1 if we reached the base case else we return 0
+like specifically here we will check 
+
+to remove overlapping subproblems we use a concept called memoization 
+to use memoization we check the chngaing parametres like here are the 2d dp 2 variable i and j 
+
+tabulation is bottom up becase we start from the base case and goes to the top accumulating the results 
+
+In space optimization whenever we see a fetching of previous row and prevoious col we can possible do space optimiztaion 
+
+To solve dp question well pehle recursion wala likh diya akro then dp laga diya karo 
+
+//Leetcode 62
+class Solution {
+public:
+
+    int solve(int i,int j,vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return 1;  //base conditions 
+        if (i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = solve(i-1,j,dp);
+        int right = solve(i,j-1,dp);
+
+        return dp[i][j] = down+right;
+
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
+};  
+  
 
 
 
