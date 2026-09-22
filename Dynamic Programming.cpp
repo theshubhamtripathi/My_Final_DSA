874afa122db334068c1a5caadc0f490d51a29fd6
Shubham Tripathi <theshubhamtripathi06@gmail.com>
2026-09-22 18:11:46 +0530

    Update Dynamic Programming.cpp

--- Dynamic Programming.cpp
+++ Dynamic Programming.cpp
@@ -353,7 +353,29 @@ public:
     }
 };  
   
+//Leetcode 1143
+In solving dp question whnever you see like you have repeating/overlappingsubproblems then use dp and also when you have options as well also yaha optimal bola hai(like max min liek this)
+class Solution {
+public:
+    int solve(string &text1, string &text2,int i,int j,vector<vector<int>> &dp){
+        if(i>=text1.length() || j>=text2.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(text1[i] == text2[j]){
+            return dp[i][j] = 1 + solve(text1,text2,i+1,j+1,dp);
+        }
+        else{
+            return dp[i][j] = max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
+        return solve(text1,text2,0,0,dp);
+    }
+};
 
+recursion plus memo 
 
 
   
